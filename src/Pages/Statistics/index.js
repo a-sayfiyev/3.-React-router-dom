2ee97968b68d1f6e67166abbb8a134db04c6ca29
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-19 13:00:33 +0500

    Add statistics page

--- src/Pages/Statistics/index.js
+++ src/Pages/Statistics/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Carousel } from "react-bootstrap";
 import CarouselImgFirst from "../../Assets/firstcarousel.jpg";
 import CarouselImgSecond from "../../Assets/carouselimg2.jpg";
