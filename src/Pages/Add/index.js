8cb62016296b947f06a4fe6133512681c2e3381c
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-19 13:00:44 +0500

    Add "Add" page

--- src/Pages/Add/index.js
+++ src/Pages/Add/index.js
@@ -7,7 +7,31 @@ export default function Add() {
       <div className="container p-4">
         <h1>Qo'shish</h1>
         <p>Yangi kategoriya/taom qo’shish</p>
-
+        <div className="main-wrapper d-flex flex-column">
+          <div className="inner-wrapper d-flex flex-column align-items-start gap-4">
+            <button
+              style={{ backgroundColor: "#FFEC00", padding: "7px 20px" }}
+              className="btn fw-bold"
+            >
+              Kategoriya
+            </button>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Kategoriya nomi"
+            />
+          </div>
+          <button
+            style={{
+              backgroundColor: "#FFEC00",
+              padding: "10px 40px",
+              margin: "0 auto",
+            }}
+            className="btn fw-bold mt-4"
+          >
+            Qo’shish
+          </button>
+        </div>
       </div>
     </>
   );
