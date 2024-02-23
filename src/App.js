cca4cc5c05423e1705bbddf971b2a7d2fc300d8e
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-23 12:18:54 +0500

    Update App.js, routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Welcome from './Pages/Welcome/index';
-import Dashboard from "./Pages/Dashboard";
+import DashboardHome from "./Pages/Dashboard";
 import Products from "./Pages/Products";
 import DashboardTemplate from "./Components/DashboardTemplate";
 import Add from "./Pages/Add";
 import Statistics from "./Pages/Statistics";
 import Users from "./Pages/Users";
+import Page404 from "./Pages/404";
+import Login from "./Pages/Login";
+import { createContext, useState } from "react";
+
+const LoginContext = createContext({isLogin: false, setIsLogin: () => {}});
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <Router>
-      <Routes>
-        {/* <Route path="/" element={<Welcome />} /> */}
-        <Route path="/" element={<DashboardTemplate />}>
-          <Route path="" element={<Dashboard/>} />
-          <Route path="products" element={<Products />} />
-          <Route path="statistics" element={<Statistics />} />
-          <Route path="add" element={<Add />} />
-          <Route path="users" element={<Users />} />
-        </Route>
-      </Routes>
+      <LoginContext.Provider value={{ isLogin, setIsLogin }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<DashboardTemplate />}>
+            <Route path="home" element={<DashboardHome />} />
+            <Route path="products" element={<Products />} />
+            <Route path="statistics" element={<Statistics />} />
+            <Route path="add" element={<Add />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </LoginContext.Provider>
     </Router>
   );
 }
 
+export {LoginContext};
 export default App;
