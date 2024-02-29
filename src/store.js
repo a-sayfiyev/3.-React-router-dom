2af864c12380590333e6288e75c48b818032b65b
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-29 11:09:41 +0500

    Update store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { initialArizalar } from "./initialArizalar";
 
 const initialState = {
   isLogin: false,
   isSidebarHidden: false,
+  arizaReducer: { arizalar: initialArizalar },
 };
 
 const store = createStore(rootReducer, initialState);
-
 
 export default store;
