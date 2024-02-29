8a9756079e960a371bdbed68f6c9441fddb2d630
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-29 11:09:34 +0500

    Update reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,62 +1,21 @@
-// reducers/index.js
 import { combineReducers } from "redux";
 import loginReducer from "./loginReducer";
 import sidebarReducer from "./sidebarReducer";
-
-const initialState = {
-  users: [
-    {
-      fullName: "Akobir Sayfiyev",
-      phone: "90 123 40 41",
-      role: "admin",
-    },
-    {
-      fullName: "Lorem Ipsumov",
-      phone: "90 123 40 41",
-      role: "yetkazuvchi",
-    },
-    {
-      fullName: "Cristiano Ronaldo",
-      phone: "90 123 40 41",
-      role: "foydalanuvchi",
-    },
-    {
-      fullName: "Leo Messi",
-      phone: "90 123 40 41",
-      role: "foydalanuvchi",
-    },
-  ],
-};
-
-const usersReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_USER_ROLE":
-      const { index, role } = action.payload;
-      const updatedUsers = [...state.users];
-      updatedUsers[index].role = role;
-      return {
-        ...state,
-        users: updatedUsers,
-      };
-    case "DELETE_USER":
-      const { deleteIndex } = action.payload;
-      const filteredUsers = state.users.filter(
-        (_, index) => index !== deleteIndex
-      );
-      return {
-        ...state,
-        users: filteredUsers,
-      };
-    default:
-      return state;
-  }
-};
+import usersReducer from "./usersReducer";
+import arizaReducer from "./arizaReducer";
 
 const rootReducer = combineReducers({
+  arizaReducer,
   isLogin: loginReducer,
   isSidebarHidden: sidebarReducer,
   users: usersReducer,
 });
 
+if (process.env.NODE_ENV === "development") {
+  const { default: ReactDevTools } = require("react-devtools");
+  ReactDevTools.loadDevTools({
+    findHostInstanceByFiber: true,
+  });
+}
 
 export default rootReducer;
