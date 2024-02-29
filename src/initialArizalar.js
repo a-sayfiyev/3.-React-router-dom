58bbbf968f95dad1874f0b37201617a67ab44f46
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-29 11:10:28 +0500

    Add initialArizalar

---
+++ src/initialArizalar.js
@@ -0,0 +1,38 @@
+export const initialArizalar = [
+  {
+    firstName: "Akobir Sayfiyev",
+    mealName: "Osh",
+    price: "20 000",
+    amount: "3",
+    overall: "overall price",
+    phone: "90 123 40 41",
+    comment: "Lorem ipsum",
+  },
+  {
+    firstName: "Lorem Ipsumov",
+    mealName: "Osh",
+    price: "20 000",
+    amount: "5",
+    overall: "overall price",
+    phone: "90 123 40 41",
+    comment: "Lorem ipsum",
+  },
+  {
+    firstName: "Ter Stegen",
+    mealName: "Manti",
+    price: "5 000",
+    amount: "10",
+    overall: "overall price",
+    phone: "90 123 40 41",
+    comment: "Lorem ipsum",
+  },
+  {
+    firstName: "Lorem Ipsumov",
+    mealName: "Big Lavash",
+    price: "24 000",
+    amount: "3",
+    overall: "overall price",
+    phone: "90 123 40 41",
+    comment: "Lorem ipsum",
+  },
+];
