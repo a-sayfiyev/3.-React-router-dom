a15abdf3b49333e3ad3e500bb5ac1f6bd505b86c
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-29 11:09:19 +0500

    Update pages

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,53 +1,14 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setLogin, toggleSidebar } from "../../actions";
+import { deleteAriza } from "../../actions";
 
 export default function Dashboard() {
-  const isLogin = useSelector((state) => state.isLogin);
-  const isSidebarHidden = useSelector((state) => state.isSidebarHidden);
+  const arizalar = useSelector((state) => state.arizaReducer.arizalar);
   const dispatch = useDispatch();
 
-  const initialArizalar = [
-    {
-      firstName: "Akobir Sayfiyev",
-      mealName: "Osh",
-      price: "20 000",
-      amount: "3",
-      overall: "overall price",
-      phone: "90 123 40 41",
-      comment: "Lorem ipsum",
-    },
-    {
-      firstName: "Lorem Ipsumov",
-      mealName: "Osh",
-      price: "20 000",
-      amount: "5",
-      overall: "overall price",
-      phone: "90 123 40 41",
-      comment: "Lorem ipsum",
-    },
-    {
-      firstName: "Ter Stegen",
-      mealName: "Manti",
-      price: "5 000",
-      amount: "10",
-      overall: "overall price",
-      phone: "90 123 40 41",
-      comment: "Lorem ipsum",
-    },
-    {
-      firstName: "Lorem Ipsumov",
-      mealName: "Big Lavash",
-      price: "24 000",
-      amount: "3",
-      overall: "overall price",
-      phone: "90 123 40 41",
-      comment: "Lorem ipsum",
-    },
-  ];
-
-  const [arizalar, setArizalar] = useState(initialArizalar);
   const [filterText, setFilterText] = useState("");
+  const [editedPrice, setEditedPrice] = useState(null);
+  const [editIndex, setEditIndex] = useState(null);
 
   const calculateOverall = (price, amount) => {
     const numericPrice = parseInt(price.replace(/\s/g, ""), 10);
@@ -56,12 +17,12 @@ export default function Dashboard() {
   };
 
   const calculateTotalOverall = () => {
+    if (!arizalar || arizalar.length === 0) return "0";
     const totalOverall = arizalar.reduce((total, ariza) => {
       const numericPrice = parseInt(ariza.price.replace(/\s/g, ""), 10);
       const numericAmount = parseInt(ariza.amount, 10);
       return total + numericPrice * numericAmount;
     }, 0);
-
     return totalOverall.toLocaleString();
   };
 
@@ -69,6 +30,21 @@ export default function Dashboard() {
     const fullName = `${ariza.firstName} ${ariza.mealName}`.toLowerCase();
     return fullName.includes(filterText.toLowerCase());
   });
+
+const handleDeleteAriza = (index) => {
+  dispatch(deleteAriza(index));
+};
+
+  const handleEditPrice = (index, price) => {
+    setEditedPrice(price);
+    setEditIndex(index);
+  };
+
+  const handleSavePrice = (index) => {
+    const updatedArizalar = [...arizalar];
+    updatedArizalar[index].price = editedPrice;
+    setEditIndex(null);
+  };
 
   return (
     <>
@@ -86,6 +62,7 @@ export default function Dashboard() {
               <th>Umumiy narxi</th>
               <th>Telefon raqami</th>
               <th>Izoh</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -94,11 +71,72 @@ export default function Dashboard() {
                 <th>{index + 1}</th>
                 <td>{ariza.firstName}</td>
                 <td>{ariza.mealName}</td>
-                <td>{ariza.price}</td>
+                <td>
+                  {editIndex === index ? (
+                    <input
+                      type="text"
+                      value={editedPrice}
+                      onChange={(e) => setEditedPrice(e.target.value)}
+                    />
+                  ) : (
+                    ariza.price
+                  )}
+                </td>
                 <td>{ariza.amount}</td>
                 <td>{calculateOverall(ariza.price, ariza.amount)}</td>
                 <td>{ariza.phone}</td>
                 <td>{ariza.comment}</td>
+                <td>
+                  {editIndex === index ? (
+                    <button
+                      className="btn"
+                      onClick={() => handleSavePrice(index)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-check2-circle"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0" />
+                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                      </svg>
+                    </button>
+                  ) : (
+                    <button
+                      className="btn"
+                      onClick={() => handleEditPrice(index, ariza.price)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-pencil-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                      </svg>
+                    </button>
+                  )}
+                  <button
+                    className="btn"
+                    onClick={() => handleDeleteAriza(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash3-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                    </svg>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setLogin, toggleSidebar } from "../../actions";
+import { deleteProduct, setLogin, toggleSidebar } from "../../actions";
 
 export default function Products() {
   const initialArizalar = [
@@ -44,6 +44,8 @@ export default function Products() {
 
   const [arizalar, setArizalar] = useState(initialArizalar);
   const [filterText, setFilterText] = useState("");
+  const [editedComment, setEditedComment] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
   const calculateOverall = (price, amount) => {
     const numericPrice = parseInt(price.replace(/\s/g, ""), 10);
@@ -61,13 +63,37 @@ export default function Products() {
     return totalOverall.toLocaleString();
   };
 
+  const handleEditComment = (index) => {
+    setEditIndex(index);
+    setEditedComment(arizalar[index].comment);
+  };
+
+  const handleSaveComment = () => {
+    const updatedArizalar = [...arizalar];
+    updatedArizalar[editIndex].comment = editedComment;
+    setArizalar(updatedArizalar);
+    setEditIndex(null);
+    setEditedComment("");
+  };
+
+  const handleCancelEdit = () => {
+    setEditIndex(null);
+    setEditedComment("");
+  };
+
+  const handleDeleteAriza = (index) => {
+    const updatedArizalar = [...arizalar];
+    updatedArizalar.splice(index, 1);
+    setArizalar(updatedArizalar);
+  };
+
   const filteredArizalar = arizalar.filter((ariza) => {
     const fullName = `${ariza.firstName} ${ariza.mealName}`.toLowerCase();
     return fullName.includes(filterText.toLowerCase());
   });
 
-  const isLogin = useSelector((state) => state.isLogin); // Access the isLogin state from Redux
-  const dispatch = useDispatch(); // Get the dispatch function
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -85,6 +111,7 @@ export default function Products() {
               <th>Umumiy narxi</th>
               <th>Telefon raqami</th>
               <th>Status</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -98,6 +125,85 @@ export default function Products() {
                 <td>{calculateOverall(ariza.price, ariza.amount)}</td>
                 <td>{ariza.phone}</td>
                 <td>{ariza.comment}</td>
+                <td>
+                  {editIndex === index ? (
+                    <>
+                      <select
+                        value={editedComment}
+                        onChange={(e) => setEditedComment(e.target.value)}
+                      >
+                        <option value="Qabul qilindi">Qabul qilindi</option>
+                        <option value="Yetkazilmoqda">Yetkazilmoqda</option>
+                        <option value="Yetkazildi">Yetkazildi</option>
+                      </select>
+                      <button
+                        className="btn btn-sm ms-2"
+                        onClick={handleSaveComment}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-check2-circle"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0" />
+                          <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                        </svg>
+                      </button>
+                      <button
+                        className="btn btn-sm ms-1"
+                        onClick={handleCancelEdit}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-x-circle"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                        </svg>{" "}
+                      </button>
+                    </>
+                  ) : (
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => handleEditComment(index)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-pencil-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                      </svg>
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    className="btn"
+                    onClick={() => handleDeleteAriza(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash3-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                    </svg>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
